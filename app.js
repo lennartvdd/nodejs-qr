a47a7afc729faa2297a5c29849f2bb3d73dd9e18
Lennart van den Dool <lennartvdd@gmail.com>
2024-12-04 16:44:16 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+
+const express = require('express');
+const QRCode = require('qrcode');
+const app = express();
+const PORT = 3000;
+
+app.get('/', async (req, res) => {
+  try {
+    const data = req.query.data || null;
+
+    if (!data) {
+      res.status(404).send("Page not found")
+      return;
+    }
+
+    const qrCodeImage = await QRCode.toBuffer(data);
+    res.setHeader('content-type', 'image/png');
+    res.send(qrCodeImage);
+  } catch (err) {
+    console.error('Error generating QR code:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
